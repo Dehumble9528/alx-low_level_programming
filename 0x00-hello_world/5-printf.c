58edaf45f73c8fead75820a119b464189a4575dd
Dehumble9528 <udoumorensunday@gmail.com>
2023-09-10 15:57:57 -0700

    returning output using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - printing output using printf
  *
  * Return: Always (0)
- * 
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+	;
 }
