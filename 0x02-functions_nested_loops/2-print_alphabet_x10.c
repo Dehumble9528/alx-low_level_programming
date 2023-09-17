345e2580add3f5945f68dec5e850f83f4fdc601a
Dehumble9528 <udoumorensunday@gmail.com>
2023-09-17 08:05:08 -0700

    printing 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int b;
+
+	for (b = 0;  b <= 10; b++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
