2450be2aa10b3277171d9843c0cbf737976f232d
Dehumble9528 <udoumorensunday@gmail.com>
2023-09-16 17:43:29 -0700

    print alphabet a-z

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef main_h
-#define main_h
+#define main_hi
 
 int _putchar(char c);
-int print_alphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
