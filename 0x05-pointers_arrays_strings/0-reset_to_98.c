e23fa563d6429c29f0176a4e1e895e547222c544
Dehumble9528 <udoumorensunday@gmail.com>
2023-09-18 18:33:02 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *reset_to_98 - pointer to an int as paramete
+ *
+ * @n: An integer input point
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
