c106492bc67ead7236a9f576f8c913f668bfed3f
Dehumble9528 <udoumorensunday@gmail.com>
2023-09-21 18:58:21 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,6 @@
  * _strncat - concatenate two strings
  * using at most n bytes from src
  * @dest: input value
- * @src: input value
  * @n: input value
  *
  * Return: dest
@@ -13,23 +12,23 @@ char *_strncat(char *dest, char *src, int n)
 
 {
 
-        int i;o
-        int j;
+				int i;
+					int j;
 
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-        j = 0;
-        while (j < n && src[j] != '\0')
-	{
-        dest[i] = src[j];
-        i++;
-        j++;
-        }
-        dest[i] = '\0';
-        return (dest);
+				i = 0;
+				while (dest[i] != '\0')
+				{
+					i++;
+				}
+				j = 0;
+				while (j < n && src[j] != '\0')
+				{
+					dest[i] = src[j];
+					i++;
+					j++;
+				}
+				dest[i] = '\0';
+				return (dest);
 }
 
 
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	 j = 0;
-        while (j < n && src[j] != '\0')
-        {
+	j = 0;
+	while (j < n && src[j] != '\0')
+				{
 		dest[j] = src[j];
-                j++;
-	}
+				j++;
+				}
 	while (j < n)
 	{
-		dest[j] = '\0';
-                j++;
-	}
+		dest[j] = j++;
 }
